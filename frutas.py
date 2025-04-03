98ef69b946c9bfbc59970aeeb0ad799579934b76
abruramirezz <raamirezqabril@gmail.com>
2025-04-03 19:01:14 -0300

    Add files via upload

---
+++ frutas.py
@@ -0,0 +1,12 @@
+'''
+frutas = ["manzana", "pera","uva", "mandarina"]
+for fruta in frutas:
+    print(fruta)
+'''    
+    
+#while #repite un bloque mientras una condicion sea verdadera 
+
+contador=0 
+while contador < 5:
+    print(contador)
+    contador +=1 
